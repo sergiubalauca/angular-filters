5b6cea2f9c0be98b70b0fa4a9669eb9f6a05eaa0
Balauca George-Sergiu <sooty2face@yahoo.com>
2021-08-24 00:30:13 +0300

    #first commit for the filters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { SelectComponent } from './dropdown/select/select.component';
+import { SearchMultiSelectComponent } from './dropdown/search-multi-select/search-multi-select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownComponent,
+    SelectComponent,
+    SearchMultiSelectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
